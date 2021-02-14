27d3578e9c9141884746d90d63a0af42f9e37e27
Ramesh Rooplahl <rameshr@DESKTOP-JFGVESR.localdomain>
2021-02-14 13:59:07 +0200

    Ramesh: Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hi there Ramesh!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React Rammie!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
